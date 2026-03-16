66dbb05f163992341d6fd545ffae5b92f417cf8f
Gonçalo Carvalho <a49219@alunos.isel.pt>
2026-03-16 22:52:46 +0000

    Primeiro commit

---
+++ src/main/kotlin/com/isel/ps/secdash/SecdashApplication.kt
@@ -0,0 +1,11 @@
+package com.isel.ps.secdash
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SecdashApplication
+
+fun main(args: Array<String>) {
+	runApplication<SecdashApplication>(*args)
+}
---
+++ src/test/kotlin/com/isel/ps/secdash/SecdashApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.isel.ps.secdash
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SecdashApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
