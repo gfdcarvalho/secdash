ccdc47268e4fb1c4999b08c75a34b269a66e0519
49448 - Rodrigo Vitorino <a49448@alunos.isel.pt>
2026-03-25 19:42:18 +0000

    Add GitHub and GitLab controllers with endpoints for repositories, vulnerabilities, and SAST

---
+++ src/main/kotlin/com/isel/ps/secdash/controller/GitlabController.kt
@@ -0,0 +1,27 @@
+package com.isel.ps.secdash.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+
+@RestController()
+@RequestMapping("/api/gitlab")
+class GitlabController {
+
+
+//    @GetMapping("/login")
+//    fun loginUser(){}
+
+    @GetMapping("/repositories")
+    fun getRepositories(){}
+
+
+    @GetMapping("/vulnerabilities")
+    fun getVulnerabilities(){}
+
+    @GetMapping("/sast")
+    fun getSast(){}
+
+
+}
