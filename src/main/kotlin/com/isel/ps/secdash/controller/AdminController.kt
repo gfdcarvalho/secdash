13ea06cb92a4c3987fef52922160c22d338ec6fd
Gonçalo Carvalho <a49219@alunos.isel.pt>
2026-06-21 18:37:31 +0100

    delete team admin controller

--- src/main/kotlin/com/isel/ps/secdash/controller/AdminController.kt
+++ src/main/kotlin/com/isel/ps/secdash/controller/AdminController.kt
@@ -1,9 +1,12 @@
 package com.isel.ps.secdash.controller
 
 import com.isel.ps.secdash.controller.model.Problem
+import com.isel.ps.secdash.model.users.AuthenticatedUser
 import com.isel.ps.secdash.service.RepositoryServices
+import com.isel.ps.secdash.service.TeamServices
 import com.isel.ps.secdash.service.UserDeletionError
 import com.isel.ps.secdash.service.UserServices
+import com.isel.ps.secdash.service.responseTypes.DeleteTeamError
 import com.isel.ps.secdash.utils.Failure
 import com.isel.ps.secdash.utils.Success
 import org.springframework.http.ResponseEntity
@@ -16,10 +19,11 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/admin")
 class AdminController(
-    private val userServices: UserServices
+    private val userServices: UserServices,
+    private val teamServices: TeamServices
 ) {
 
-    @DeleteMapping("/delete/{uid}")
+    @DeleteMapping("/delete-user/{uid}")
     @PreAuthorize("hasRole('ADMIN')") // isto funciona o user não tem que estar authenticato ?
     fun deleteUser(
         @PathVariable uid: Int
@@ -30,6 +34,22 @@ class AdminController(
                 UserDeletionError.UserNotFound -> Problem.response(404, Problem.UserNotFound)
                 UserDeletionError.Forbidden -> Problem.response(403, Problem.forbidden)
             }
+        }
+    }
+
+    @DeleteMapping("/delete-team/{teamId}")
+    fun deleteTeam(
+        user: AuthenticatedUser,
+        @PathVariable teamId: Int
+    ): ResponseEntity<*> {
+        val result = teamServices.deleteTeam(user.user.uid, user.user.role, teamId)
+        return when (result) {
+            is Success -> ResponseEntity.noContent().build<Any>()
+            is Failure ->
+                when (result.value) {
+                    DeleteTeamError.TeamNotFound -> Problem.response(404, Problem.teamNotFound)
+                    DeleteTeamError.OnlyTeamLeaderOrAdmin -> Problem.response(401, Problem.onlyTeamLeader)
+                }
         }
     }
 
